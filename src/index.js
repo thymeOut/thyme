9e651d4e42ca16e08d17988d8fa4b4d1cead6e0d
Matthew Baker <matthewbaker@Matthews-MBP.lan>
2021-07-01 17:27:41 -0400

    adding some graphql functionality

--- src/index.js
+++ src/index.js
@@ -13,17 +13,15 @@ const client = new ApolloClient({
   resolvers: {},
 });
 
+
+
+
 ReactDOM.render(
   <Router>
     <ApolloProvider client={client}>
+    
       <App />
     </ApolloProvider>
   </Router>,
-  document.getElementById("app")
+  document.getElementById("root")
 );
-
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
