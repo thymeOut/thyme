337245bc92a9223188b4034019d53f704dad08e4
eeevvv <evan.w.and@gmail.com>
2021-07-03 22:49:05 -0400

    feat: single container view renders user names and item names, and can be navigated to from UserContainer component

---
+++ client/components/SingleContainer.js
@@ -0,0 +1,61 @@
+import React, { Fragment, useContext, useEffect, useState } from "react";
+import { useQuery, gql, useMutation } from "@apollo/client";
+
+const GET_CONTAINER = gql`
+  query Container($id: ID!) {
+    container(id: $id) {
+      id
+      name
+      users {
+        id
+        firstName
+        lastName
+      }
+      items {
+        name
+        imageUrl
+      }
+    }
+  }
+`;
+
+export default function SingleContainer(props) {
+  const containerId = props.match.params.id;
+
+  const { loading, error, data } = useQuery(GET_CONTAINER, {
+    variables: {
+      id: containerId,
+    },
+  });
+
+  if (loading) return "...loading";
+  if (error) return "...error";
+
+  return (
+    <div>
+      <h2>{data.container.name}</h2>
+      <h3>Users</h3>
+      <div>
+        {data.container.users.map((user) => {
+          return (
+            <div key={user.id}>
+              {user.firstName} {user.lastName}
+            </div>
+          );
+        })}
+      </div>
+      <h3>Contents</h3>
+      <div>
+        {
+          data.container.items.map(item => {
+            return (
+              <div key={item.id}>
+                {item.name}
+              </div>
+            )
+          })
+        }
+      </div>
+    </div>
+  );
+}
--- client/components/UserContainers.js
+++ client/components/UserContainers.js
@@ -1,37 +1,40 @@
 import React, { Fragment, useContext, useEffect, useState } from "react";
 import { useQuery, gql, useMutation } from "@apollo/client";
+import { Link } from "react-router-dom";
 
 const GET_CONTAINERS = gql`
-   query User ($id: ID!) {
+  query User($id: ID!) {
     user(id: $id) {
       id
-      containers{
-          id
-          name
+      containers {
+        id
+        name
       }
     }
-   }
-`
+  }
+`;
 
 export default function UserContainers() {
-    console.log(localStorage.getItem('user-id'))
-    const { loading, error, data } = useQuery(GET_CONTAINERS,
-        {
-            variables: {
-                id: localStorage.getItem('user-id')
-            }
-        })
-    if (loading) return '...loading'
-    if (error) return '...error'
-    console.log(data)
-    return (
-        <div>
-            <h2>
-                My Containers
-            </h2>
-            <div>
-                {data && data.user.containers.map(container => <div key={container.id}>{container.name}</div>)}
-            </div>
-        </div>
-    )
+  console.log(localStorage.getItem("user-id"));
+  const { loading, error, data } = useQuery(GET_CONTAINERS, {
+    variables: {
+      id: localStorage.getItem("user-id"),
+    },
+  });
+  if (loading) return "...loading";
+  if (error) return "...error";
+  console.log(data);
+  return (
+    <div>
+      <h2>My Containers</h2>
+      <div>
+        {data &&
+          data.user.containers.map((container) => (
+            <Link to={`/containers/${container.id}`}>
+              <div key={container.id}>{container.name}</div>
+            </Link>
+          ))}
+      </div>
+    </div>
+  );
 }
