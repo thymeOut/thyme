af1e3cba8a9dba9ca1728bd56d5e85bcec9f6424
eeevvv <evan.w.and@gmail.com>
2021-07-06 15:33:42 -0400

    fix: move all seed data into modular files

---
+++ script/data.js
@@ -0,0 +1,13 @@
+const users = require('./users');
+const containers = require('./containers');
+const items = require('./items');
+const containerUsers = require('./containerUsers');
+const containerItems = require('./containerItems');
+
+module.exports = {
+  users,
+  containers,
+  items,
+  containerUsers,
+  containerItems,
+};
