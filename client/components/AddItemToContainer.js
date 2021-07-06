cb8c08f10dce3eb0414dd9514d6802d8cb69eb73
eeevvv <evan.w.and@gmail.com>
2021-07-06 17:33:03 -0400

    connect AddItem component to react router and link to it from SingleContainer component

--- client/components/AddItemToContainer.js
+++ client/components/AddItemToContainer.js
@@ -2,3 +2,35 @@ import React, { Fragment, useContext, useEffect, useState } from 'react';
 import { useQuery, gql, useMutation } from '@apollo/client';
 import { Link } from 'react-router-dom';
 
+const GET_ITEMS = gql`
+  query Items {
+    items {
+      id
+      name
+      imageUrl
+    }
+  }
+`;
+
+export default function AddItemToContainer() {
+  const { loading, error, data } = useQuery(GET_ITEMS);
+
+  if (loading) {
+    return '...loading';
+  }
+
+  if (error) {
+    return '...error';
+  }
+
+  return (
+    <React.Fragment>
+      <h2>Items to choose from:</h2>
+      <div>
+        {data.items.map((item) => {
+          return <div key={item.id}>{item.name}</div>;
+        })}
+      </div>
+    </React.Fragment>
+  );
+}
