89b25d247b3c2a1be1a40bb7755ab5be476f179a
Ugene <ugeneromashov@yahoo.com>
2021-07-06 15:06:01 -0400

    search feature

--- client/components/UserContainers.js
+++ client/components/UserContainers.js
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
-import { useQuery, gql, useMutation } from '@apollo/client';
-import { Link } from 'react-router-dom';
-import ContainerForm from './ContainerForm';
+import React, { useState } from "react";
+import { useQuery, gql, useMutation } from "@apollo/client";
+import { Link } from "react-router-dom";
+import ContainerForm from "./ContainerForm";
+import JoinContainer from "./JoinContainer";
 
 const GET_CONTAINERS = gql`
   query User($id: ID!) {
@@ -25,11 +26,12 @@ const CREATE_CONTAINER = gql`
 `;
 
 export default function UserContainers() {
-  const [toggle, setToggle] = useState(false);
+  const [createToggle, setCreateToggle] = useState(false);
+  const [joinToggle, setJoinToggle] = useState(false);
 
   const { loading, error, data } = useQuery(GET_CONTAINERS, {
     variables: {
-      id: localStorage.getItem('user-id'),
+      id: localStorage.getItem("user-id"),
     },
   });
 
@@ -38,14 +40,14 @@ export default function UserContainers() {
       {
         query: GET_CONTAINERS,
         variables: {
-          id: localStorage.getItem('user-id'),
+          id: localStorage.getItem("user-id"),
         },
       },
     ],
   });
 
-  if (loading) return '...loading';
-  if (error) return '...error';
+  if (loading) return "...loading";
+  if (error) return "...error";
 
   return (
     <div>
@@ -57,13 +59,18 @@ export default function UserContainers() {
               <div>{container.name}</div>
             </Link>
           ))}
-        <button onClick={() => setToggle(true)}> Add new container</button>
-        {toggle && (
+        <button onClick={() => setCreateToggle(true)}>
+          {" "}
+          Add new container
+        </button>
+        {createToggle && (
           <ContainerForm
             createContainer={createContainer}
-            setToggle={setToggle}
+            setCreateToggle={setCreateToggle}
           />
         )}
+        <button onClick={() => setJoinToggle(true)}> Join a container</button>
+        {joinToggle && <JoinContainer setJoinToggle={setJoinToggle} />}
       </div>
     </div>
   );
