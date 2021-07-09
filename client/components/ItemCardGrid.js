fde436a0e203baf571dae54054ab122286fc08a5
eeevvv <evan.w.and@gmail.com>
2021-07-09 13:04:58 -0400

    fix: add ItemCardGrid.js changes

--- client/components/ItemCardGrid.js
+++ client/components/ItemCardGrid.js
@@ -23,48 +23,7 @@ export default function ItemCardGrid(props) {
     <Container className={classes.cardGrid} maxWidth="md">
       <Grid container spacing={4}>
         {items.map((item) => (
-          <Grid item key={item.id} xs={12} sm={6} md={4}>
-            <Card className={classes.card}>
-              <CardMedia
-                className={classes.cardMedia}
-                image={item.imageUrl}
-                title={item.name}
-              />
-              <CardContent className={classes.cardContent}>
-                <Typography gutterBottom variant="h5" component="h2">
-                  {item.name}
-                </Typography>
-                <Typography>
-                  {users.map((user) => {
-                    if (user.id === item.containerItem.userId) {
-                      return user.firstName;
-                    }
-                  })}
-                </Typography>
-                <Typography>
-                  {item.containerItem.expiration
-                    ? `Expiration: ${item.containerItem.expiration}`
-                    : ''}
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <ButtonGroup size="small" color="primary">
-                  <Button>-</Button>
-                  <Button>
-                    {item.containerItem.originalQuantity -
-                      item.containerItem.quantityUsed}
-                  </Button>
-                  <Button>+</Button>
-                </ButtonGroup>
-                <Button size="small" color="primary">
-                  Edit
-                </Button>
-                <Button size="small" color="primary">
-                  Remove
-                </Button>
-              </CardActions>
-            </Card>
-          </Grid>
+          <ItemCard item={item} classes={classes} users={users} />
         ))}
       </Grid>
     </Container>
