dc9779b8d1ff6b2f7e0acdd58d3306ee49fea8b6
matt-shake-n-baker <72667632+matt-shake-n-baker@users.noreply.github.com>
2021-07-14 21:51:27 -0400

    fix: link stuff

--- client/index.js
+++ client/index.js
@@ -37,7 +37,7 @@ const authLink = setContext((_, { headers }) => {
 });
 
 const client = new ApolloClient({
-  link,
+  link: authLink.concat(httpLink,errorLink),
   cache: new InMemoryCache(),
   resolvers: {},
 });
