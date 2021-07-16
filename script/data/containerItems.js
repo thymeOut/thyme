7411474217c9a089bbed69c4c547cd64bdcd1fcb
eeevvv <evan.w.and@gmail.com>
2021-07-16 17:22:00 -0400

    add more expired container items, increase container item instances to 1000, and randomly generate quantityUsed for some container item instances

--- script/data/containerItems.js
+++ script/data/containerItems.js
@@ -2,7 +2,7 @@ const faker = require('faker');
 
 let containerItems = [];
 
-for (let i = 0; i < 300; i++) {
+for (let i = 0; i < 1000; i++) {
   let newContainerItem = {
     itemId: Math.ceil(Math.random() * 24),
     originalQuantity: Math.ceil(Math.random() * 30),
@@ -10,9 +10,14 @@ for (let i = 0; i < 300; i++) {
   };
 
   let dateTrue = Math.floor(Math.random() * 2);
+  let rand2 = Math.floor(Math.random() * 2);
+
+  if (rand2) {
+    newContainerItem.quantityUsed = Math.floor(Math.random() * newContainerItem.originalQuantity);
+  }
 
   if (dateTrue) {
-    const expiration = faker.date.between('2021-07-10', '2021-09-22');
+    const expiration = faker.date.between('2020-09-10', '2021-09-22');
 
     newContainerItem.expiration = expiration;
 
