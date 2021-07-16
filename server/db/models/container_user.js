100cfa9e594b2a4ed9f4b5bb6373b9ef076f2f6b
eeevvv <evan.w.and@gmail.com>
2021-07-16 15:33:33 -0400

    feat: pending users appear in single container view and can be accepted or rejected

--- server/db/models/container_user.js
+++ server/db/models/container_user.js
@@ -2,6 +2,13 @@ const Sequelize = require("sequelize");
 const db = require("../db");
 
 const ContainerUser = db.define("containerUser", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+    unique: true
+  },
   role: {
     type: Sequelize.ENUM('user', 'owner', 'pending'),
     allowNull: false,
