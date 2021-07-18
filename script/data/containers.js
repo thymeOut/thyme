a524bd7609fc3420cb10fc6cabf604d94e78977e
eeevvv <evan.w.and@gmail.com>
2021-07-18 13:49:02 -0400

    fix: add image URLs for diff. container types to seed script

--- script/data/containers.js
+++ script/data/containers.js
@@ -10,6 +10,7 @@ let containers = [
     name: "Jane's Pantry",
     type: 'pantry',
     ownerId: 2,
+    imageUrl: 'https://www.closetcreationsri.com/_images/pantry.png',
   },
 ];
 
@@ -21,6 +22,17 @@ for (let i = 0; i < 100; i++) {
     type: containerTypes[Math.floor(Math.random() * 4)],
     ownerId: Math.floor(Math.random() * 104),
   };
+
+  if (newContainer.type === 'pantry') {
+    newContainer.imageUrl =
+      'https://www.closetcreationsri.com/_images/pantry.png';
+  } else if (newContainer.type === 'freezer') {
+    newContainer.imageUrl =
+      'https://smhttp-ssl-87202.nexcesscdn.net/pub/media/catalog/product/cache/90b96709c3e472eb35f4c1eef9a9f5f1/u/h/uhfz124-ss01a_1.jpg';
+  } else if (newContainer.type === 'minifridge') {
+    newContainer.imageUrl =
+      'https://images.thdstatic.com/productImages/bc530048-aaa8-4118-b5a0-91d4a26e78aa/svn/red-galanz-mini-fridges-glr31trder-e1_600.jpg';
+  }
 
   containers.push(newContainer);
 }
